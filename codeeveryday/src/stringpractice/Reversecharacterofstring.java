49be7fefa199c2be2e0f28c4c4203689babe4b70
Akankshya Pany <panysayeni@gmail.com>
2025-12-18 15:40:46 +0530

    day 1 commit

---
+++ codeeveryday/src/stringpractice/Reversecharacterofstring.java
@@ -0,0 +1,25 @@
+package stringpractice;
+
+public class Reversecharacterofstring {
+
+	public static void main(String[] args) {
+	 String str=" siii rd  ";
+	 String result = reverseString(str);
+     System.out.println(result);
+	}
+
+	private static String reverseString(String str) {
+		char[] array = str.trim().toCharArray(); //covertinf string to char array
+		int start=0;
+		int end=array.length-1;
+		while(start<=end) {
+			char temp =array[start];
+			array[start] =array[end];
+		    array[end] = temp;
+			start++;
+			end--;
+		}
+		return new String(array); //char array to string
+	}
+
+}
