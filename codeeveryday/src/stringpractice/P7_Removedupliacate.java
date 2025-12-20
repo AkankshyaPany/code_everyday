c984e9643d72dd3c89bad1e9d9f0c2aa330d7379
Akankshya Pany <panysayeni@gmail.com>
2025-12-20 12:51:35 +0530

    Day 2 practice

--- codeeveryday/src/stringpractice/P7_Removedupliacate.java
+++ codeeveryday/src/stringpractice/P7_Removedupliacate.java
@@ -1,10 +1,28 @@
 package stringpractice;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class P7_Removedupliacate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "dixkjmcce";
+		char[] array = str.toCharArray();
+		System.out.println(array);
+		System.out.println(Arrays.toString(array));
+		
+		HashSet<Character> set = new HashSet<>();
+		for(char ch : array) {
+			set.add(ch);
+		}
+		System.out.println(set);
+		
+		//set to string
+		StringBuilder s= new StringBuilder();
+		for(char ch : array) {
+			s.append(ch);
+		}
+		System.out.println(s);
 	}
-
+	
 }
